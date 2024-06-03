f4aa19bde9570c91cf79a0312050a40cf91289b5
Tamanna Chourasia <tamannachaurasiya17@gmail.com>
2024-06-03 12:24:16 +0530

    Update socket.js

--- routes/socket.js
+++ routes/socket.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const User = require('./models/user');
+// const express = require('express');
+// const router = express.Router();
+// const User = require('./models/user');
 
-router.post('/join', (req, res) => {
-  const { email, name, socketId } = req.body;
-  const newUser = new User({ email, name, socketId });
-  newUser.save()
-    .then(() => res.status(201).send('User joined the room'))
-    .catch(err => res.status(400).send('Error joining the room: ' + err));
-});
+// router.post('/join', (req, res) => {
+//   const { email, name, socketId } = req.body;
+//   const newUser = new User({ email, name, socketId });
+//   newUser.save()
+//     .then(() => res.status(201).send('User joined the room'))
+//     .catch(err => res.status(400).send('Error joining the room: ' + err));
+// });
 
-router.get('/users', (req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).send('Error fetching users: ' + err));
-});
+// router.get('/users', (req, res) => {
+//   User.find()
+//     .then(users => res.json(users))
+//     .catch(err => res.status(400).send('Error fetching users: ' + err));
+// });
 
-module.exports = router;
+// module.exports = router;
