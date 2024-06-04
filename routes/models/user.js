5e7ee2f102abe3f0c228c15a2eaf925ba53960ee
Tamanna Chourasia <tamannachaurasiya17@gmail.com>
2024-06-04 13:14:28 +0530

    Update user.js

--- routes/models/user.js
+++ routes/models/user.js
@@ -1,35 +1,5 @@
  const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   firstName: { type: String, required: true },
-//   lastName: { type: String, required: true },
-//   mobileNo: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   address: {
-//     street: { type: String, required: true },
-//     city: { type: String, required: true },
-//     state: { type: String, required: true },
-//     country: { type: String, required: true },
-//   },
-//   loginId: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   socketId: { type: String },
-// });
-
-// const User = mongoose.model('User', userSchema);
-// const userSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   mobileNo: String,
-//   email: String,
-//   street: String,
-//   city: String,
-//   state: String,
-//   country: String,
-//   loginId: String,
-//   password: String
-// });
-
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
@@ -44,8 +14,10 @@ const userSchema = new mongoose.Schema({
   loginId: { type: String, required: true, match: /^[a-zA-Z0-9]{8,}$/ },
   password: { type: String, required: true, match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/ },
   creationTime: { type: Date, default: Date.now },
-  lastUpdatedOn: { type: Date, default: Date.now }
+  lastUpdatedOn: { type: Date, default: Date.now },
+  socketId: { type: String}
 });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
+
