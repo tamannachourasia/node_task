d7acbd09f3c411af3be235cde14ed775c8eb1d51
Tamanna Chourasia <tamannachaurasiya17@gmail.com>
2024-06-05 15:29:45 +0530

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -3,21 +3,41 @@ const router = express.Router();
 const User = require('./models/user');
 
 // POST request to create a new user
+// POST request to create a new user
 router.post('/create', async function (req, res) {
-  
+  const { firstName, lastName, email, mobileNo, address, loginId, password } = req.body;
+  const socketId = req.io.sockets.sockets.keys().next().value; // Assign socketId if available
+
+  if (!socketId) {
+    return res.status(400).send('Socket ID not available');
+  }
+
   try {
-    const user = new User(req.body);
+    const user = new User({ 
+      firstName, 
+      lastName, 
+      email, 
+      mobileNo,
+      address: {
+        street: address.street,
+        city: address.city,
+        state: address.state,
+        country: address.country
+      },
+      loginId,
+      password,
+      socketId 
+    });
     await user.save();
-    const liveUsers = req.liveUsers; // Access liveUsers from req object
-    const io = req.io; // Access io from req object
-    liveUsers[user.socketId] = user;
-    io.emit('new-user', user);
+    req.liveUsers[user.socketId] = user;
+    req.io.emit('new-user', user);
     res.status(201).send('User created successfully');
   } catch (err) {
-    console.error('Error creating user:', err); 
+    console.error('Error creating user:', err);
     res.status(500).send(err.message);
   }
 });
+
 
 
 // GET request to fetch users
